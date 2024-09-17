5591b0b735abf863183e8ff2ee72a662987dd121
redaei <redaeis@gmail.com>
2024-09-17 20:43:29 +0300

    update

--- app.js
+++ app.js
@@ -275,8 +275,8 @@ This function should return the count of arguments passed to it when called.
 Complete the exercise in the space below:
 */
 
-function numArgs() {
-  return arguments.length
+const numArgs = (...num) => {
+  return num.length
 }
 
 console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4))
